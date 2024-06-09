9d47d79962d85cd5e414da38a69970064af0ed95
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-06-09 17:29:55 -0400

    Backend Setup Begin

---
+++ Masterless Gaming/backend/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+// Middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started on port 3000!!!");
+});
