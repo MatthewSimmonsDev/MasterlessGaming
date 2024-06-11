8754ae13d01d81ba51a68b6a27275660b8c46cca
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-06-11 10:35:27 -0400

    Updates

---
+++ components/Description.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <>
+      <p className="text-lg text-gray-700 mb-4 max-w-3xl mx-auto text-center mt-16">
+        Masterless Gaming is a web application that will allow users to
+        expeience solo, co-op or guided tabletop role-playing in a virtual
+        environment.
+      </p>
+    </>
+  );
+};
+
+export default Description;
