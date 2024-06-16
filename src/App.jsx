16d0cad1c30c3165520ba87de90b3ff6a644266c
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-06-16 14:08:20 -0400

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Title from "./components/Title";
 import Description from "./components/Description";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
       <Title />
       <Description />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <h3 className='text-lg text-black-700 mb-4 max-w-3xl mx-auto text-center mt-16'>
+    &copy;2024  Iron Mettle Games MatthewSimmonsDev@gmail.com</h3>
+    </>
+  )
+}
+
+export default Footer
